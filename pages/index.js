efd933a502a13c659ae5d2df5ec5c93be917b299
Rockmeister56 <megacake.records@gmail.com>
2026-02-06 03:58:46 -0800

    4

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 50 }}>
+      <h1>VidGuide AI is Ready! 🎬</h1>
+      <p>Your video interview platform is being built.</p>
+      <p>Next: Connect to Supabase database</p>
+    </div>
+  )
+}
